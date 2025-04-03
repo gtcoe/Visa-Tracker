cd57eaa3ed6b5219b8c94cc32217b63b34b40014
Garvit Tyagi <garvittyagicoe@gmail.com>
2025-04-04 01:19:25 +0530

    search api implemented

---
+++ app/src/config/request/application/updateApplication.ts
@@ -0,0 +1,42 @@
+import { ValidationSchema } from "../interface/request";
+import { APPLICATION_EXTERNAL_STATUS, APPLICATION_QUEUES } from "../../applicationStatus";
+
+const validationSchema: ValidationSchema = {
+  body: {
+    id: {
+      type: "number",
+      required: true,
+      minValue: 1,
+    },
+    queue: {
+      type: "number",
+      required: true,
+      minValue: APPLICATION_QUEUES.IN_TRANSIT,
+      maxValue: APPLICATION_QUEUES.BILLING,
+    },
+    external_status: {
+      type: "number",
+      required: true,
+      minValue: APPLICATION_EXTERNAL_STATUS.DOC_RECIVED,
+      maxValue: APPLICATION_EXTERNAL_STATUS.PASSPORT_DISPATCHED_APPROVAL_AWAITED,
+    },
+    team_remarks: {
+      type: "string",
+      required: false,
+    },
+    client_remarks: {
+      type: "string",
+      required: false,
+    },
+    billing_remarks: {
+      type: "string",
+      required: false,
+    },
+    token_user_id: {
+      type: "number",
+      required: true,
+    },
+  },
+};
+
+export default validationSchema; 
